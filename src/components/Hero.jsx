70e3fa73c67554bf85a84980e040d71d334a472b
Pascal <tux0riz0r@gmail.com>
2024-01-24 22:16:35 +0100

    pictures

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,8 +25,8 @@ const Hero = () => {
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img
           src={robot}
-          alt='billing'
-          className='w-[100%] h-[100%] relative z-[5]'
+          alt='lock'
+          className='w-[100%] h-[100%] relative z-[5] rounded-full absolute opacity-80 brightness-130'
         />
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
